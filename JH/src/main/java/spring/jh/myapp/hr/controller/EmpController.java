a1570a238a60ee65cd35ddf5b8944419a3174918
Juhyub Lee <scall24@naver.com>
2020-06-04 12:56:05 +0900

    Spring day3 HOME

--- JH/src/main/java/spring/jh/myapp/hr/controller/EmpController.java
+++ JH/src/main/java/spring/jh/myapp/hr/controller/EmpController.java
@@ -1,9 +1,14 @@
 package spring.jh.myapp.hr.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import spring.jh.myapp.hr.dao.EmpRepository;
 import spring.jh.myapp.hr.dao.IEmpService;
@@ -11,15 +16,35 @@ import spring.jh.myapp.hr.model.EmpVO;
 
 
 @Controller
+@RequestMapping("/hr")
 public class EmpController {
 
 	@Autowired
 	IEmpService empService;
 	
-	@GetMapping("/hr/juhyub")
-	public String getUser(Model model) {
-		model.addAttribute("user", empService.getEmpInfo(102));
-		return "home";
+	@RequestMapping("/count")
+	public String empCount(@RequestParam(value="deptId",
+	required=false, defaultValue="0") int deptId, Model model) {
+		if(deptId==0) {
+			model.addAttribute("count", empService.getEmpCount());
+		} else {
+			model.addAttribute("count", empService.getEmpCount(deptId));
+		}
+		return "hr/count";
+	}
+	
+	@RequestMapping("/list")
+	public String getAllEmployees(Model model) {
+		List<EmpVO> empList = empService.getEmpList();
+		model.addAttribute("empList", empList);
+		return "hr/list";
+	}
+	
+	@RequestMapping("/{employeeId}")
+	public String getEmployees(@PathVariable int employeeId, Model model) {
+		EmpVO emp = empService.getEmpInfo(employeeId);
+		model.addAttribute("emp",emp);
+		return "hr/view";
 	}
 	
 	
